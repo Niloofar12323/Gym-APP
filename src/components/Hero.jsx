e40e8a1f1a2741eb2693f033cee06aab147a98e1
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2024-12-05 05:50:59 +0100

    1

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
+        <p>IT'S TIME TO GET  </p>
+         <h1>Swoles <span>normous</span></h1>
+         <p>I hereby acknowledgement that I may become <span>unbelievably swolenormous </span>and accept all risks of becoming the local <span>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>    
+      
+    </div>
+  )
+}
+
+
