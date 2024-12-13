bc6c8c5a9326c5b5db70264ed51a119c264603e0
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2024-12-13 23:51:16 +0100

    1

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionWrapper from './SectionWrapper'
-import { WORKOUTS } from '../utils/swoldier'
+import { SCHEMES, WORKOUTS } from '../utils/swoldier'
 
 function Header(props){
   const {index, title, description} = props
@@ -16,6 +16,13 @@ function Header(props){
 }
 
 export default function () {
+  const [showModal, setShowModal] = useState(false)
+  const [poison,setPoison] = useState('individual')
+  const [muscles,setMuscles] = useState([])
+  const [goals,setGoals] = useState('strength_power')
+ function toggleModal() {
+  setShowModal(!showModal)
+}
   return (
     
    <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge','o\'clock']}>
@@ -32,14 +39,28 @@ export default function () {
   })}
   </div>
   <Header index={'02'} title={'Lock on targets'} description={"Select the muscles judged for annihilation."}/>
-  <div className='bg-slate-950 p-3 border border-solid border-blue-400 rounded-lg'>
-<div>
+  <div className='bg-slate-950 p-3 border border-solid border-blue-400 rounded-lg flex flex-col'>
+<button onClick={toggleModal} className='relative p-3 flex items-center justify-center'>
   <p>Select muscle groups</p>
-  <i className='fa-solid fa-caret-down'></i>
-</div>
+  <i className='fa-solid absolute  right-3 top-1/2 -trenslate-y-1/2 fa-caret-down'></i>
+</button>
   
-  
+  {showModal && (
+    <div>modal</div>
+  )}
  
+  </div>
+  <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to endure."}/>
+  <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
+
+  
+  {Object.keys(SCHEMES).map((scheme, schemeIndex)=>{
+    return (
+       <button className='bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg' key={schemeIndex}>
+        <p className='capitalize'>{scheme.replaceAll('_', " ")}</p>
+       </button>
+    )
+  })}
   </div>
    </SectionWrapper>
   )
