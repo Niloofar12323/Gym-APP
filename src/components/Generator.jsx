d190c84ba7148eefd820c7aa07bacfe83ccde8fd
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2024-12-22 15:42:27 +0100

    1

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -24,20 +24,27 @@ export default function () {
   setShowModal(!showModal)
 }
 
-function updateMuscles(){
-  if (muscles.length > 2){
-    return
-  }
-  if (poison !== 'individual'){
-    setMuscles([muscleGroup])
-    return
+function updateMuscles(muscleGroup) {
+  if (muscles.includes(muscleGroup)) {
+      setMuscles(muscles.filter(val => val !== muscleGroup))
+      return
   }
 
-  if(muscles.includes(muscleGroup)){
-    setMuscles(muscles.filter(val=>val !== muscleGroup))
-    return
+  if (muscles.length > 2) {
+      return
   }
-  setMuscles([...muscles,muscleGroup])
+
+  if (poison !== 'individual') {
+      setMuscles([muscleGroup])
+      setShowModal(false)
+      return
+  }
+
+  setMuscles([...muscles, muscleGroup])
+  if (muscles.length === 2) {
+      setShowModal(false)
+  }
+
 }
   return (
     
@@ -49,6 +56,7 @@ function updateMuscles(){
   {Object.keys(WORKOUTS).map((type, typeIndex)=>{
     return (
        <button onClick={()=>{
+        setMuscles([])
         setPoison(type)
        }} className={'bg-slate-950 border  duration-200 hover:border-blue-600 py-3 rounded-lg ' + (type === poison ? ' border-blue-600' : 'border-blue-400')} key={typeIndex}>
         <p className='capitalize'>{type.replaceAll('_', " ")}</p>
@@ -59,7 +67,7 @@ function updateMuscles(){
   <Header index={'02'} title={'Lock on targets'} description={"Select the muscles judged for annihilation."}/>
   <div className='bg-slate-950 p-3 border border-solid border-blue-400 rounded-lg flex flex-col'>
 <button onClick={toggleModal} className='relative p-3 flex items-center justify-center'>
-  <p>Select muscle groups</p>
+  <p className='capitalize'>{muscles.length==0 ?'Select muscle groups' : muscles.join(' ')}</p>
   <i className='fa-solid absolute  right-3 top-1/2 -trenslate-y-1/2 fa-caret-down'></i>
 </button>
   
@@ -67,11 +75,11 @@ function updateMuscles(){
     <div className='flex flex-col px-3 pb-3 '>
       {(poison === 'individual' ? WORKOUTS[poison]:Object.keys(WORKOUTS[poison])).map((muscleGroup,muscleGroupIndex) =>{
         return(
-            <button onClick={()=>{ updateMuscles(muscleGroup)}} key={muscleGroupIndex} className={'hover:text-blue-400 duration-200' +(muscles.includes(muscleGroup) ? 'text-blue-400' : ' ')} >
-              <p className='uppercase'>{muscleGroup.replaceAll('_', '')}
-              </p>
-
-            </button>
+             <button onClick={() => {
+                                    updateMuscles(muscleGroup)
+                                }} key={muscleGroupIndex} className={'hover:text-blue-400 duration-200 ' + (muscles.includes(muscleGroup) ? ' text-blue-400' : ' ')}>
+                                    <p className='uppercase'>{muscleGroup.replaceAll('_', ' ')}</p>
+                                </button>
         )
       })}
 
