4056736ed5262ab114449aa6793eb92d4d74818d
niloofar yousefiafrouz <niloofar.yousefiafrouz@dci-student.org>
2025-01-10 22:49:57 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   setWorkout(newWorkout)
 
-  window.location.href= "#workout"
+  window.location.href= '#workout'
  }
   return (
     <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
